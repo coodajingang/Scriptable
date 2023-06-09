09fe1421da90909af5aa455eb0fdcffe9428a4e8
coodajingang <coodajingang@outlook.com>
2023-06-09 23:09:31 +0800

    modify wg

--- test_wg.js
+++ test_wg.js
@@ -5,6 +5,16 @@ var PREFER_TRANSPARENT_BG = false
 var nobg = !PREFER_TRANSPARENT_BG ? null 
     : await importModuleOptional('no-background')
 
+const bz = await importModuleOptional('bundleprod')
+log('Import bazi bundleprod.js')
+
+const yearGZ = '丙寅'
+const monthGZ = '戊戌'
+const dayGZ = '丙戌'
+const hourGZ = '辛卯'
+const yunGZ = '辛丑'
+
+
 async function createWidget(widgetFamily='large') {  
   
   const widget = new ListWidget()
@@ -17,44 +27,49 @@ async function createWidget(widgetFamily='large') {
 
   const foreColor = Color.white()
 
+  const now = new Date();
+  const bazi = bz.calendarDayInfo(now.getFullYear(), now.getMonth()+1, now.getDate(), 3);
+  const hour = now.getHours();
+  const hGZ = bazi.bazi.hs[Math.floor((hour+1)/2)]
+
   const headStack = widget.addStack()
   headStack.centerAlignContent()
   headStack.layoutHorizontally()
   headStack.setPadding(0,0,0,0)
-  headStack.addDate(new Date())
+  headStack.addDate(now)
   headStack.addSpacer()
-  headStack.addText("nongtli")
+  headStack.addText(bazi.nontli)
   const mainStack = widget.addStack()
   const tailStack = widget.addStack()
   tailStack.setPadding(0,0,0,0)
   tailStack.centerAlignContent()
   tailStack.layoutHorizontally()
-  tailStack.addText("节气。。。。")
+  tailStack.addText(genJieQi(bazi, 0))
   tailStack.addSpacer()
-  tailStack.addText("nongtli")
+  tailStack.addText(genJieQi(bazi, 0))
 
   mainStack.setPadding(0,0,0,0)
   mainStack.centerAlignContent()
   mainStack.layoutHorizontally()
 
   mainStack.addSpacer()
-  addDZ(mainStack, '甲', '寅')
+  addDZ(mainStack, yearGZ[0], yearGZ[1])
   mainStack.addSpacer()
-  addDZ(mainStack, '甲', '寅')
+  addDZ(mainStack, monthGZ[0], monthGZ[1])
   mainStack.addSpacer()
-  addDZ(mainStack, '甲', '寅')
+  addDZ(mainStack, dayGZ[0], dayGZ[1])
   mainStack.addSpacer()
-  addDZ(mainStack, '甲', '寅')
+  addDZ(mainStack, hourGZ[0], hourGZ[1])
   mainStack.addSpacer()
-  addDZ(mainStack, '甲', '寅')
+  addDZ(mainStack, yunGZ[0], yunGZ[1])
   mainStack.addSpacer()
-  addDZ(mainStack, '甲', '寅')
+  addDZ(mainStack, bazi.bazi.year[0], bazi.bazi.year[1])
   mainStack.addSpacer()
-  addDZ(mainStack, '甲', '寅')
+  addDZ(mainStack, bazi.bazi.month[0], bazi.bazi.month[1])
   mainStack.addSpacer()
-  addDZ(mainStack, '甲', '寅')
+  addDZ(mainStack, bazi.bazi.day[0], bazi.bazi.day[1])
   mainStack.addSpacer()
-  addDZ(mainStack, '甲', '寅')
+  addDZ(mainStack, hGZ[0], hGZ[1])
   mainStack.addSpacer()
   return widget
 }
@@ -71,7 +86,6 @@ function addDZ(mainStack, first, second) {
     st.centerAlignText()
     st.font = Font.title1()
 }
-
 
 
 //---[ main ]-------------------------------------
@@ -98,8 +112,16 @@ if (config.runsInWidget) {
         break;
       default:   
     }
-  }
+}
 
+function genJieQi(bazi, i) {
+    if (!bazi || !bazi.solar || !bazi.solar.length || bazi.solar.length <= i) {
+        log('No solar info ' + i)
+        return ''
+    }
+    const d = bazi.solar[i]
+    return d.name + ":" + d.day  + '日' + d.time
+}
 //--公共方法库-------------------
 function sfIcon(name, font) {
     const sf = SFSymbol.named(name)
@@ -152,9 +174,7 @@ async function downloadImage(url) {
 async function importModuleOptional(module_name) {
     const ICLOUD =  module.filename
                       .includes('Documents/iCloud~')
-    const fm = FileManager[ICLOUD 
-                            ? 'iCloud' 
-                            : 'local']()
+    const fm = FileManager()
     if (!/\.js$/.test(module_name)) {
       module_name = module_name + '.js'
     }
